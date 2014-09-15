877391932db037a49647c3a65eae80505e29601f
Henrik <henrik.renlund@ucr.uu.se>
2014-09-15 10:01:15 +0200

    State of files as of 2014vt.

---
+++ _META_.r
@@ -0,0 +1,29 @@
+library(knitr)
+library(Hmisc)
+library(RColorBrewer)
+library(graphics)
+
+opts_chunk$set(
+   cache=TRUE, # default: FALSE
+   #include=TRUE, # default: TRUE
+   echo=FALSE, # default: TRUE
+   message=FALSE, # default: TRUE
+   error=FALSE, # default: TRUE
+   warning=FALSE, # default: TRUE
+   fig.align='center',
+   fig.width=11,
+   fig.height=5
+)
+
+knit2pdf("L9.rnw")
+# knit("L6.rnw")
+
+knit2pdf("L6.rnw")
+# knit("L6.rnw")
+
+knit2pdf("L1and2.rnw")
+# knit("L1and2.rnw")
+
+proh::clean()
+proh::killCache()
+# rm(list=setdiff(ls(), "set_params"))
